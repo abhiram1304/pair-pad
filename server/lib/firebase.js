d26d14ea15e7a689aebe7286f9146a05a9cf0305
abhiram1304 <atmuriabhiram@gmail.com>
2025-06-13 19:54:12 -0500

    fix(firebase): use FIREBASE_CREDS_B64 in production

--- server/lib/firebase.js
+++ server/lib/firebase.js
@@ -1,11 +1,19 @@
 const admin = require('firebase-admin');
 
-const creds = process.env.FIREBASE_CREDS
-  ? JSON.parse(process.env.FIREBASE_CREDS)
-  : require('../../serviceAccountKey.json');
+let creds;
 
-admin.initializeApp({
-  credential: admin.credential.cert(creds),
-});
+if (process.env.FIREBASE_CREDS_B64) {
+  creds = JSON.parse(Buffer.from(process.env.FIREBASE_CREDS_B64, 'base64').toString());
+} else {
+  try {
+    creds = require('../../serviceAccountKey.json');
+  } catch {
+    throw new Error(
+      'Firebase credentials not found. Set FIREBASE_CREDS_B64 in the environment.'
+    );
+  }
+}
+
+admin.initializeApp({ credential: admin.credential.cert(creds) });
 
 module.exports = admin;
